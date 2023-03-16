424b9999b5ea8bea5d83a746a2036c66cefb7332
Mariangela Gomez <100443251+mariangela-gomez@users.noreply.github.com>
2023-03-16 12:17:41 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,17 @@
+var boton = document.getElementById("boton");
+var textoOculto = document.querySelector(".oculto");
+var textoVisible = document.querySelector(".texto");
+
+boton.addEventListener("click", function() {
+  if (textoOculto.style.display === "none") {
+    textoOculto.style.display = "block";
+    textoVisible.style.height = "140vh";
+    boton.innerHTML = "Mostrar menos";
+  } else {
+    textoOculto.style.display = "none";
+    textoVisible.style.height = "65vh";
+    boton.innerHTML = "Mostrar más";
+  }
+});
+
+
