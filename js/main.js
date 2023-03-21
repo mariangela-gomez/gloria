949c5f5085eb26dd45e545eca1308b08574a50fa
Mariangela Gomez <100443251+mariangela-gomez@users.noreply.github.com>
2023-03-21 16:22:01 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,11 +5,11 @@ var textoVisible = document.querySelector(".texto");
 boton.addEventListener("click", function() {
   if (textoOculto.style.display === "none") {
     textoOculto.style.display = "block";
-    textoVisible.style.height = "150vh";
+    textoVisible.style.height = "170vh";
     boton.innerHTML = "Mostrar menos";
   } else {
     textoOculto.style.display = "none";
-    textoVisible.style.height = "65vh";
+    textoVisible.style.height = "80vh";
     boton.innerHTML = "Mostrar más";
   }
 });
